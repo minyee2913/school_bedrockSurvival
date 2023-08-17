cc94ef74ac40ede1218b53db92ff5bad09b56540
minyee2913 <leegm0759@gmail.com>
2023-08-17 14:23:14 +0900

    timeset

--- scripts/system/announce.ts
+++ scripts/system/announce.ts
@@ -1,9 +1,36 @@
 import { bedrockServer } from "bdsx/launcher";
 import { command } from "../../bdsx/command";
 import * as _ from "lodash";
+import * as schedule from "node-schedule";
+import { events } from "bdsx/event";
 
-command.register("announce", "fortest").overload((p, o, out)=>{
+const jobs: schedule.Job[] = [];
+
+jobs[0] = schedule.scheduleJob("* 30 8 * *", ()=>{
     _(bedrockServer.level.getPlayers()).forEach((player)=>{
-        player.sendToastRequest("§l§7시간표 알림", "§l1교시가 시작되었습니다.");
+        player.sendToastRequest("§7시간표 알림", "10분 동안 조회 시간입니다.");
     });
-}, {});
+});
+
+let time = 1;
+for (let i = 1; i <= 12; i+=2) {
+    jobs[i] = schedule.scheduleJob(`* 30 ${7 + time} * *`, ()=>{
+        _(bedrockServer.level.getPlayers()).forEach((player)=>{
+            player.sendToastRequest("§7시간표 알림", "10분 동안 조회 시간입니다.");
+        });
+    });
+
+    jobs[i + 1] = schedule.scheduleJob(`* 40 ${7 + time} * *`, ()=>{
+        _(bedrockServer.level.getPlayers()).forEach((player)=>{
+            player.sendToastRequest("§7시간표 알림", `${time}교시가 시작됩니다.`);
+        });
+    });
+
+    time++;
+}
+
+events.serverLeave.on(()=>{
+    jobs.forEach((v)=>{
+        v.cancel();
+    });
+});
