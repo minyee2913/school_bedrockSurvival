a1eb0e68eb76219230cc876b893d663b7b9d6c00
minyee2913 <leegm0759@gmail.com>
2023-08-17 13:55:14 +0900

    base system

--- scripts/system/announce.ts
+++ scripts/system/announce.ts
@@ -4,6 +4,6 @@ import * as _ from "lodash";
 
 command.register("announce", "fortest").overload((p, o, out)=>{
     _(bedrockServer.level.getPlayers()).forEach((player)=>{
-        player.sendToastRequest("");
+        player.sendToastRequest("§l§7시간표 알림", "§l1교시가 시작되었습니다.");
     });
 }, {});
--- scripts/system/index.ts
+++ scripts/system/index.ts
@@ -1,1 +1,2 @@
 import "./announce";
+import "./base";
