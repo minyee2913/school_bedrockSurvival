1438dd2a3097b60a8f995ef0075ef50913b6dc11
minyee2913 <leegm0759@gmail.com>
2023-08-17 14:42:46 +0900

    change time

--- scripts/system/announce.ts
+++ scripts/system/announce.ts
@@ -13,7 +13,7 @@ jobs[0] = schedule.scheduleJob("* 30 8 * *", ()=>{
 });
 
 let time = 1;
-for (let i = 1; i <= 12; i+=2) {
+for (let i = 1; i <= 8; i+=2) {
     jobs[i] = schedule.scheduleJob(`* 30 ${7 + time} * *`, ()=>{
         _(bedrockServer.level.getPlayers()).forEach((player)=>{
             player.sendToastRequest("§7시간표 알림", "10분 동안 조회 시간입니다.");
@@ -21,6 +21,34 @@ for (let i = 1; i <= 12; i+=2) {
     });
 
     jobs[i + 1] = schedule.scheduleJob(`* 40 ${7 + time} * *`, ()=>{
+        _(bedrockServer.level.getPlayers()).forEach((player)=>{
+            player.sendToastRequest("§7시간표 알림", `${time}교시가 시작됩니다.`);
+        });
+    });
+
+    time++;
+}
+
+jobs[jobs.length] = schedule.scheduleJob(`* 30 12 * *`, ()=>{
+    _(bedrockServer.level.getPlayers()).forEach((player)=>{
+        player.sendToastRequest("§7시간표 알림", "10분 동안 조회 시간입니다.");
+    });
+});
+
+jobs[jobs.length] = schedule.scheduleJob(`* 30 13 * *`, ()=>{
+    _(bedrockServer.level.getPlayers()).forEach((player)=>{
+        player.sendToastRequest("§7시간표 알림", "10분 동안 조회 시간입니다.");
+    });
+});
+
+for (let i = 1; i <= 4; i+=2) {
+    jobs[6 + i] = schedule.scheduleJob(`* 30 ${9 + time} * *`, ()=>{
+        _(bedrockServer.level.getPlayers()).forEach((player)=>{
+            player.sendToastRequest("§7시간표 알림", "10분 동안 조회 시간입니다.");
+        });
+    });
+
+    jobs[i + 7] = schedule.scheduleJob(`* 40 ${9 + time} * *`, ()=>{
         _(bedrockServer.level.getPlayers()).forEach((player)=>{
             player.sendToastRequest("§7시간표 알림", `${time}교시가 시작됩니다.`);
         });
