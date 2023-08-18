9247b9c41326067baf3bec10b55dba2e0ec52685
minyee2913 <leegm0759@gmail.com>
2023-08-18 09:30:44 +0900

    add bread

--- scripts/system/notice.ts
+++ scripts/system/notice.ts
@@ -18,11 +18,12 @@ events.playerJoin.on((ev)=>{
             ev.player.addTag("alreadyJoin");
 
             ev.player.runCommand("give @s iron_axe");
+            ev.player.runCommand("give @s bread 8");
 
             const result = await Form.sendTo(ev.player.getNetworkIdentifier(), {
                 type: "modal",
                 title: "§l환영",
-                content: "§l선린 서바이벌에 오신 것을 환영합니다!\n\n이곳에서는 무엇을 해도 상관없습니다! 분쟁! 싸움! 전부 알아서 해결하세요!\n치트 사용이 꺼져있으며 게임 플레이에 재미를 위한 특수한 기능이 몇가지 존재합니다!\n\n그럼 즐겁게 플레이하세요!\n\n§7made by minyee2913",
+                content: "선린 서바이벌에 오신 것을 환영합니다!\n\n이곳에서는 무엇을 해도 상관없습니다! 분쟁! 싸움! 전부 알아서 해결하세요!\n치트 사용이 꺼져있으며 게임 플레이에 재미를 위한 특수한 기능이 몇가지 존재합니다!\n\n그럼 즐겁게 플레이하세요!\n\n§7made by minyee2913",
                 button1: "확인",
                 button2: "취소"
             });
