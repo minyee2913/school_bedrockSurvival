a6824bb8a035779d0c10bcaab8fa7c69c5b1372d
minyee2913 <leegm0759@gmail.com>
2023-08-23 08:31:17 +0900

    fix

--- scripts/system/init.ts
+++ scripts/system/init.ts
@@ -2,6 +2,7 @@ import { events } from "bdsx/event";
 import { bedrockServer } from "bdsx/launcher";
 import { applyDash } from "./dash";
 import { applyMenu } from "./menu";
+import { AttributeId } from "bdsx/bds/attribute";
 
 events.playerJoin.on((ev)=>{
     bedrockServer.executeCommand("gamerule sendcommandfeedback false");
@@ -9,12 +10,24 @@ events.playerJoin.on((ev)=>{
     bedrockServer.executeCommand("gamerule dodaylightcycle false");
     bedrockServer.executeCommand("gamerule keepinventory true");
 
+    const slots = ev.player.getInventory().container.getSlots();
+    slots.toArray().forEach((v)=>{
+        if (v.getCustomName().startsWith("§l")) v.setAmount(0);
+    });
+
     applyDash(ev.player);
     applyMenu(ev.player);
+
+    const hp = ev.player.getAttributes().getMutableInstance(AttributeId.Health);
+    if (hp) {
+        hp.minValue = -1;
+        hp.maxValue = 30;
+    }
 });
 
 events.playerRespawn.on((ev)=>{
     ev.player.runCommand("replaceitem entity @s slot.hotbar 0 iron_axe");
+
     applyDash(ev.player);
     applyMenu(ev.player);
 });
